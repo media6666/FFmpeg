d508e20403cea0bdc21db5a838e92f2f77ec1143
Media6666 <media6666@gmail.com>
2025-11-21 15:49:50 +0700

    Test log

--- app/src/main/java/com/ffmpeg/media/MainActivity.kt
+++ app/src/main/java/com/ffmpeg/media/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.ffmpeg.media
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.e("AAAA", "Test log")
     }
 }
